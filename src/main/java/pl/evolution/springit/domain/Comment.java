995625dbb0ceb64a5721e9bff1dd06cb622bad74
barik90 <barik.rafal@gmail.com>
2018-12-05 14:39:02 +0100

    Update application.properties with basic h2

--- src/main/java/pl/evolution/springit/domain/Comment.java
+++ src/main/java/pl/evolution/springit/domain/Comment.java
@@ -3,10 +3,7 @@ package pl.evolution.springit.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 
 
 @Entity
@@ -19,7 +16,7 @@ public class Comment extends Auditable{
     private Long id;
     private String body;
 
-    @ManyToMany
+    @ManyToOne
     private Link link;
 
 }
