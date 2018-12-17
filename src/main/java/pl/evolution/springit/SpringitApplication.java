eb0fac095a626fe659a2860b5daec4cd68ce4824
barik90 <barik.rafal@gmail.com>
2018-12-17 12:51:52 +0100

    Add templates, bootstrap and change in domain and controller

--- src/main/java/pl/evolution/springit/SpringitApplication.java
+++ src/main/java/pl/evolution/springit/SpringitApplication.java
@@ -1,16 +1,12 @@
 package pl.evolution.springit;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import pl.evolution.springit.domain.Comment;
-import pl.evolution.springit.domain.Link;
-import pl.evolution.springit.repository.CommentRepository;
-import pl.evolution.springit.repository.LinkRepository;
 
 
 @SpringBootApplication
@@ -23,21 +19,9 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
+
     @Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-        return args -> {
-            Link link = new Link("Getting Started with Spring Boot 2", "http://therealdanvvega.com/spring-boot-2");
-            linkRepository.save(link);
-
-            Link link2 = new Link("Just do it!", "http://nike.com");
-            linkRepository.save(link2);
-
-            Comment comment = new Comment("This Spring boot 2 link is awesome!", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-            System.out.println("We just insert a link and a comment");
-
-        };
+    PrettyTime prettyTime(){
+        return new PrettyTime();
     }
 }
